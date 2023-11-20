9958103a00f172bf03f6cce6ba80aa7a0aa389ba
Nishad <nishadkindre9553@gmail.com>
2023-11-20 19:09:36 +0530

    added

---
+++ array_methods.js
@@ -0,0 +1,30 @@
+let marks_class_12 = [91, 82, 63, 84, false, "Not Present"]
+console.log(marks_class_12[0])
+console.log(marks_class_12[1])
+console.log(marks_class_12[2])
+console.log(marks_class_12[3])
+console.log(marks_class_12[4])
+console.log(marks_class_12[5])
+console.log(marks_class_12[6]) // Will be undefined because index 6 does not exist
+console.log("The length of marks_class_12 is", marks_class_12.length)
+marks_class_12[6] = 89 // Adding a new value to the array
+marks_class_12[0] = 96 // Changing the value of an array
+console.log(marks_class_12)
+console.log(typeof marks_class_12)
+
+// Array Methods
+let num = [1, 2, 3, 34, 4]
+let b = num.toString() // b is now a string 
+console.log(b, typeof b)
+let c = num.join(" and ")
+console.log(c, typeof c)
+// let r = num.pop() // pop returns the popped element
+// console.log(num, r)
+// let r = num.push(56) // push returns the new array length
+// console.log(num, r)
+// let r = num.shift()
+// console.log(r, num) // Removes an element from the start of the array
+
+let r = num.unshift(78)
+console.log(r, num)
+console.log(r)
---
+++ sum_of_numbers.js
@@ -0,0 +1,6 @@
+a = prompt("Enter the number - ")
+sum0f = 0
+for (i = 1; i <= Number(a); i++){
+    sumof += i
+}
+console.log(sum0f)
